de862ef49f32524d8c28cce2d56be8d83462e07e
Sodiq Ogundairo <kingmclamba1@gmail.com>
2025-05-22 14:08:02 +0100

    responsivenes updated

--- src/pages/HomeNew.jsx
+++ src/pages/HomeNew.jsx
@@ -16,10 +16,10 @@ const HomeNew = () => {
         
       </main> */}
 
-    <main className="flex flex-col items-center justify-center text-center gap-3 lg:gap-4 ">
+    <main className="flex flex-col items-center justify-center text-center gap-3 lg:gap-4 mt-4 lg:mt-2 ">
       {/* <div className="flex flex-col items-center justify-center text-center gap-6 lg:gap-12 mt-6 lg:mt-12 pt-5 md:pt-10"> */}
 
-      <h1 className="text-bolder text-primary text-4xl font-bold w-4/5 md:text-5xl md:w-3/5 lg:text-5xl lg:w-3/5 ">
+      <h1 className="text-bolder text-primary text-4xl font-bold w-4/5 md:text-5xl md:w-3/5 lg:text-5xl lg:w-3/5 xl:text-6xl">
         Share trips with the <br className="hidden md:block" /> ones you love
       </h1>
 
@@ -29,7 +29,7 @@ const HomeNew = () => {
         expenses—all in one place.
       </p>
 
-      <div className="flex flex-col md:flex-row justify-between gap-6">
+      <div className="flex justify-between gap-2">
         <a href="https://play.google.com/store/apps/details?id=com.outout.mobileapp" className="bg-dark text-light flex justify-between gap-2 items-center rounded-md px-4 py-2">
           <img src={GooglePlayLogo} alt="appleLogo" />
           <span className="flex flex-col justify-start items-start leading-tight">
