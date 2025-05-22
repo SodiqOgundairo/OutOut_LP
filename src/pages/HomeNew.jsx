e2fb39ddd5dd6c6245a4afe95a4287f78205724f
Sodiq Ogundairo <kingmclamba1@gmail.com>
2025-05-22 12:52:27 +0100

    responsivenes updated

--- src/pages/HomeNew.jsx
+++ src/pages/HomeNew.jsx
@@ -1,3 +1,4 @@
+
 import Header from "../components/Header"
 import AppleLogo from "./../assets/img/AppleLogo.svg"
 import GooglePlayLogo from "./../assets/img/GooglePlayLogo.svg"
@@ -6,24 +7,25 @@ import bgOne from "./../assets/img/bg-one.png"
 import bgTwo from "./../assets/img/bg-two.png"
 
 const HomeNew = () => {
-  return (
-    <main className="flex flex-col items-center justify-between text-center h-screen overflow-hidden">
-      <Header className="w-screen"/>
-      {/* <div className="flex flex-col items-center justify-center gap-8 text-center my-5">
 
-      </div> */}
+  return (
+    <div className="">
+      <Header />
+
+    <main className="flex flex-col items-center justify-center text-center ">
+      <div className="flex flex-col items-center justify-center text-center gap-12 mt-12 pt-10">
+
       <h1 className="text-bolder text-primary text-4xl font-bold w-4/5 md:text-5xl md:w-3/5 lg:text-6xl lg:w-3/5 ">
-      {/* <h1 className="text-bolder text-primary text-3xl lg:text-7xl font-bold w-4/5 lg:w-2/5"> */}
-        Share trips with the <br /> ones you love
+        Share trips with the <br className="hidden md:block" /> ones you love
       </h1>
 
-      <p className="text-primary w-4/5 lg:w-4/5 ">
+      <p className="text-primary w-4/5 lg:w-3/5 ">
         A weekend getaway with your best friends or Thursday dinner with the
-        crew? Vote on dates, <br />chat with your group, share photos, and split
+        crew? Vote on dates, chat with your group, share photos, and split
         expenses—all in one place.
       </p>
 
-      <div className="flex justify-between gap-2 md:gap-6">
+      <div className="flex flex-col md:flex-row justify-between gap-6">
         <a href="https://play.google.com/store/apps/details?id=com.outout.mobileapp" className="bg-dark text-light flex justify-between gap-2 items-center rounded-md px-4 py-2">
           <img src={GooglePlayLogo} alt="appleLogo" />
           <span className="flex flex-col justify-start items-start leading-tight">
@@ -41,10 +43,12 @@ const HomeNew = () => {
         </a>
       </div>
 
-      <img src={OutOutMockup} alt="OutOutMockup" className="md:w-2/5 z-50 "/>
+      </div>
+      <img src={OutOutMockup} alt="OutOutMockup" className="md:w-2/5 z-50 mx-auto absolute bottom-0"/>
+    </main>
       <img src={bgOne} alt="doodles" className="absolute bottom-0 right-0"/>
       <img src={bgTwo} alt="doodles" className="absolute bottom-0 left-0"/>
-    </main>
+    </div>
   )
 }
 
